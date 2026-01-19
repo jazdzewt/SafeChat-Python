8453c36774882dbfc4c69effc7fba9438a3d2314
jazdzewt <jazdzewskitymon@gmail.com>
2026-01-19 18:45:17 +0100

    Dodanie komentarzy w crypto_utils.py.

--- web/crypto_utils.py
+++ web/crypto_utils.py
@@ -1,6 +1,7 @@
 import os
 import argon2
 import base64
+
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
@@ -26,11 +27,9 @@ def verify_password(hash: str, password: str) -> bool:
     except argon2.exceptions.VerifyMismatchError:
         return False
 
+# Generowanie pary kluczy RSA 2048-bit (lucz prywatny szyfrowany hasłem)
 def generate_key_pair(password: str):
-    """
-    Generuje parę kluczy RSA 2048-bit.
-    Klucz prywatny jest SZYFROWANY hasłem użytkownika (AES-256).
-    """
+
     private_key = rsa.generate_private_key(
         #standardowy wykładnik publiczny e
         public_exponent=65537,
@@ -56,55 +55,50 @@ def generate_key_pair(password: str):
     
     return encrypted_private_pem, public_pem
 
+# Odszyfrowanie klucza prywatnego
 def decrypt_private_key(encrypted_pem_data: bytes, password: str):
-    """
-    Ładuje (odszyfrowuje) klucz prywatny RSA z formatu PEM używając hasła.
-    """
+
     try:
         private_key = serialization.load_pem_private_key(
             encrypted_pem_data,
             password=password.encode(),
         )
         return private_key
-    except Exception as e:
-        print(f"Private keys decryption failed: {e}")
+    except Exception:
         return None
 
+# Szyfrowanie sekretu TOTP użytkownika hasłem (PBKDF2 + AES-128 CBC i HMAC-SHA256 Fernet)
 def encrypt_totp(data: str, password: str) -> bytes:
-    """
-    Szyfruje dowolny tekst (np. sekret TOTP) używając hasła użytkownika.
-    Używa PBKDF2 do wyprowadzenia klucza dla AES (Fernet).
-    """
 
     salt = os.urandom(16)
     
-    # 2. Derive key
+    # Wyprowadzenie klucza z hasła i soli
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
         salt=salt,
         iterations=100000,
     )
+    # Wynikowo 32 bajty (wymóg biblioteki Fernet)
+    
+    # Wyprowadzenie klucza zakodowane w base64 (wymóg biblioteki Fernet)
     key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
     
-    # 3. Encrypt
+    # Szyfrowanie
     f = Fernet(key)
     token = f.encrypt(data.encode())
     
-    # 4. Return salt + token (aby móc odszyfrować potrzebujemy tej soli)
-    # Format: salt(16b) + token
+    # salt (16 bajtów) + token
     return salt + token
 
+# Odszyfrowanie sekretu TOTP 
 def decrypt_totp(encrypted_data: bytes, password: str) -> str:
-    """
-    Odszyfrowuje dane (np. sekret TOTP).
-    Wyciąga sól z pierwszych 16 bajtów, generuje klucz i odszyfrowuje.
-    """
-    # 1. Extract salt
+
+    # Wyciągnięcie danych
     salt = encrypted_data[:16]
     token = encrypted_data[16:]
     
-    # 2. Derive key (musi być identycznie jak przy szyfrowaniu)
+    # Wyprowadzenie klucza (tak jak przy szyfrowaniu)
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=32,
@@ -113,77 +107,59 @@ def decrypt_totp(encrypted_data: bytes, password: str) -> str:
     )
     key = base64.urlsafe_b64encode(kdf.derive(password.encode()))
     
-    # 3. Decrypt
+    # Odszyfrowanie
     f = Fernet(key)
     try:
         decrypted_bytes = f.decrypt(token)
         return decrypted_bytes.decode('utf-8')
-    except Exception as e:
-        print(f"Decryption failed: {e}")
+    except Exception:
         return None
 
+# Szyfrowanie tekstu wiadomosci i załączników za pomocą AES-256 GCM
 def encrypt_aes_gcm(session_key: bytes,plaintext: bytes):
-    """
-    Szyfruje dane algorytmem AES-GCM.
-    Zwraca: (ciphertext, nonce, tag, session_key)
-    """
-    #session_key = key #os.urandom(32) # 256-bit AES key
-    nonce = os.urandom(12) # 96-bit nonce for GCM
 
+    #przekazany klucz sesyjny ma długość 32 bajtów - 256-bit
+
+    nonce = os.urandom(12) # 96-bit nonce (number generated once) dla GCM
+
+    # obiekt szyfrowania
     cipher = Cipher( algorithms.AES(session_key), modes.GCM(nonce), backend=default_backend() )
     encryptor = cipher.encryptor()
     
+    # szyfrowanie, najpierw dane (plaintext), następnie kończymy i obliczamy tag (finalize)
     ciphertext = encryptor.update(plaintext) + encryptor.finalize()
     
     return ciphertext, nonce, encryptor.tag, session_key
 
+# Odszyfrowanie tekstu wiadomosci i załączników za pomocą AES-256 GCM
 def decrypt_aes_gcm(session_key: bytes, ciphertext: bytes, nonce: bytes, tag: bytes) -> bytes:
-    """
-    Odszyfrowuje dane algorytmem AES-GCM.
-    """
 
     cipher = Cipher(algorithms.AES(session_key), modes.GCM(nonce, tag), backend=default_backend())
     decryptor = cipher.decryptor()
     
     return decryptor.update(ciphertext) + decryptor.finalize()
 
+# Szyfrowanie klucza sesyjnego za pomocą RSA (OAEP + MGF1 + SHA256)
 def encrypt_rsa(public_key_pem: str, data: bytes) -> bytes:
-    """
-    Szyfruje dane (np. klucz sesyjny 32B) kluczem PUBLICZNYM (PEM).
-    Używa OAEP + MGF1 + SHA256 (standard).
-    """
+
     public_key = serialization.load_pem_public_key(public_key_pem.encode())
     
     encrypted = public_key.encrypt(
         data,
+        # algorytm dopełnienia OAEP
         padding.OAEP(
+            # Mask Generation Function - MGF1 (z SHA256 jako silnik mieszajacy)
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            # Algorytm mieszania danych (SHA256)
             algorithm=hashes.SHA256(),
             label=None
         )
     )
     return encrypted
 
-def sign_rsa(private_key, data: bytes) -> bytes:
-    """
-    Podpisuje dane (np. hash wiadomości) kluczem PRYWATNYM.
-    Używa PSS + MGF1 + SHA256.
-    """
-    signature = private_key.sign(
-        data,
-        padding.PSS(
-            mgf=padding.MGF1(hashes.SHA256()),
-            salt_length=padding.PSS.MAX_LENGTH
-        ),
-        hashes.SHA256()
-    )
-    return signature
-
+# Oszyfrowywanie klucza sesyjnego za pomocą RSA (OAEP + MGF1 + SHA256)
 def decrypt_rsa(private_key, ciphertext: bytes) -> bytes:
-    """
-    Odszyfrowuje dane (np. klucz sesyjny) używając klucza PRYWATNEGO.
-    Używa OAEP + MGF1 + SHA256.
-    """
+
     plaintext = private_key.decrypt(
         ciphertext,
         padding.OAEP(
@@ -194,10 +170,22 @@ def decrypt_rsa(private_key, ciphertext: bytes) -> bytes:
     )
     return plaintext
 
+# Podpisanie zaszyfrowanych danych za pomocą RSA (PSS + MGF1 + SHA256)
+def sign_rsa(private_key, data: bytes) -> bytes:
+
+    signature = private_key.sign(
+        data,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
+    return signature
+
+# Weryfikacja podpisu/sygnatury zaszyfrowanych danych za pomocą RSA (PSS + MGF1 + SHA256)
 def verify_signature_rsa(public_key_pem: str, data: bytes, signature: bytes) -> bool:
-    """
-    Weryfikuje podpis danych kluczem PUBLICZNYM.
-    """
+
     try:
         public_key = serialization.load_pem_public_key(public_key_pem.encode())
         public_key.verify(
@@ -210,6 +198,5 @@ def verify_signature_rsa(public_key_pem: str, data: bytes, signature: bytes) -> 
             hashes.SHA256()
         )
         return True
-    except Exception as e:
-        print(f"Signature verification failed!")
+    except Exception:
         return False
